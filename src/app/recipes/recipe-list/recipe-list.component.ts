26fead5ffc7207e6547ae46e47e8c1c73d7bbce6
Ryan Filax-Wylie <ryanfilaxwylie@gmail.com>
2020-03-27 08:06:42 -0600

    Styled ingredients component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,11 +8,7 @@ import { Recipe } from "./recipe.model";
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe(
-      "A test recipe",
-      "Simple test",
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTUuqdTj0Ei2IsEHBYMMyqK866YU7M8Milb3iIxbsLSvo6lJ2bd"
-    )
+    new Recipe("A test recipe", "Simple test", "https://robohash.org/1")
   ];
   constructor() {}
 
